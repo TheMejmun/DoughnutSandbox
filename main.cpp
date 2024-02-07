6a53e87b4097b7d92d934d2ed2347e4b2cc15a43
Saman Miran <c@samanmiran.com>
2024-02-07 12:49:46 +0100

    switch back to vcpkg, but for parent project

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include "util/importer.h"
+#include "graphics/renderer_v2.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    auto test = Doughnut::importMesh("");
+    Doughnut::Window window{"Hello World"};
+    Doughnut::RendererV2 renderer{&window, "Hello World"};
+
     return 0;
 }
