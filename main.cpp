0c3778b23866dd2d183ada8911c433a266606be8
Saman Miran <c@samanmiran.com>
2024-02-10 01:32:59 +0100

    Swapchain Image and ImageViews

--- main.cpp
+++ main.cpp
@@ -2,9 +2,31 @@
 #include "util/importer.h"
 #include "graphics/renderer_v2.h"
 
+class Object {
+public:
+    explicit Object(uint32_t i) : i(i) {
+        std::cout << "Constructed" << std::endl;
+    };
+
+    Object(const Object &other): i(other.i) {
+        std::cout << "Copied" << std::endl;
+    };
+
+    Object(Object &&other) noexcept: i(other.i) {
+        std::cout << "Moved" << std::endl;
+    };
+
+    ~Object() {
+        std::cout << "Destroyed" << std::endl;
+    }
+
+private:
+    uint32_t i;
+};
+
 int main() {
 #ifndef NDEBUG
-    dn::log::init(true, true, false, true);
+    dn::log::init(true, true, true, true);
 #else
     dn::log::init(true, false, false, true);
 #endif
