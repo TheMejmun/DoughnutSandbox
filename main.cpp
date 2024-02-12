d1304646a688b4ce6da8b6347201e91f771baf6f
Saman Miran <c@samanmiran.com>
2024-02-13 00:38:31 +0100

    acquire image

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "util/importer.h"
 #include "graphics/renderer_v2.h"
+#include "util/timer.h"
 
 int main() {
 #ifndef NDEBUG
@@ -13,11 +14,23 @@ int main() {
         dn::Window window{"Hello World"};
         dn::RendererV2 renderer{window};
 
+        dn::Time lastTimestamp = dn::now();
+        double delta;
+        while (!window.shouldClose()) {
+            dn::Time time = dn::now();
+            delta = dn::duration(lastTimestamp, time);
+            lastTimestamp = time;
+
+            renderer.drawFrame(delta);
+            dn::log::flush();
+        }
+
     } catch (const std::exception &e) {
         dn::log::e(e.what());
         dn::log::flush();
         return EXIT_FAILURE;
     }
 
+    dn::log::flush();
     return 0;
 }
