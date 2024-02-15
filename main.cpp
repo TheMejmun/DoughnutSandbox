febaccda3320c82cd1588cc32eaebea32c2deacf
Saman Miran <c@samanmiran.com>
2024-02-16 00:45:20 +0100

    Draw frame

--- main.cpp
+++ main.cpp
@@ -10,27 +10,18 @@ int main() {
     dn::log::init(true, false, false, true);
 #endif
 
-    try {
-        dn::Window window{"Hello World"};
-        dn::RendererV2 renderer{window};
+    dn::Window window{"Hello World"};
+    dn::RendererV2 renderer{window};
 
-        dn::Time lastTimestamp = dn::now();
-        double delta;
-        while (!window.shouldClose()) {
-            dn::Time time = dn::now();
-            delta = dn::duration(lastTimestamp, time);
-            lastTimestamp = time;
+    dn::Time lastTimestamp = dn::now();
+    double delta;
+    while (!window.shouldClose()) {
+        dn::Time time = dn::now();
+        delta = dn::duration(lastTimestamp, time);
+        lastTimestamp = time;
 
-            renderer.drawFrame(delta);
-            dn::log::flush();
-        }
-
-    } catch (const std::exception &e) {
-        dn::log::e(e.what());
-        dn::log::flush();
-        return EXIT_FAILURE;
+        renderer.drawFrame(delta);
     }
 
-    dn::log::flush();
     return 0;
 }
